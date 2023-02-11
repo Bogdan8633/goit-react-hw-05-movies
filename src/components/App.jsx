bd72bcc2dfc1239a41e9b6399ff23a302a50f26f
Bogdan <bogdanberezka@gmail.com>
2023-02-11 17:05:16 +0200

    replace-pages-directory-and-paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from 'modules/Navbar/Navbar';
 
-import HomePage from 'modules/Navbar/pages/HomePage/HomePage';
-import MoviesPage from 'modules/Navbar/pages/MoviesPage/MoviesPage';
-import NotFoundPage from 'modules/Navbar/pages/NotFoundPage/NotFoundPage';
+import HomePage from 'pages/HomePage/HomePage';
+import MoviesPage from 'pages/MoviesPage/MoviesPage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 
 export const App = () => {
   return (
