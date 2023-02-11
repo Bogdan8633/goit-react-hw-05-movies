9343878710909a195a2a370f951472731c342dfb
Bogdan <bogdanberezka@gmail.com>
2023-02-11 19:57:23 +0200

    add-singleMoviePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Navbar from 'modules/Navbar/Navbar';
 import HomePage from 'pages/HomePage/HomePage';
 import MoviesPage from 'pages/MoviesPage/MoviesPage';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+import SingleMoviePage from 'pages/HomePage/SinglePost/SingleMoviePage';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/movies/:id" element={<SingleMoviePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
---
+++ src/pages/HomePage/SinglePost/SingleMoviePage.jsx
@@ -0,0 +1,24 @@
+const SingleMoviePage = () => {
+  return (
+    <>
+      <div>
+        <div>Picture</div>
+        <h2>Movie title</h2>
+        <p>
+          User score: <span>100%</span>
+        </p>
+        <h3>Overview</h3>
+        <p>Overview description</p>
+        <h3>Genres</h3>
+        <p>Genres list</p>
+      </div>
+      <div>
+        <p>Additional information</p>
+        <p>Cast</p>
+        <p>Reviews</p>
+      </div>
+    </>
+  );
+};
+
+export default SingleMoviePage;
