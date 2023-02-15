d28d8c7a96f957d0c35114c4293e6a95074c7c0d
Bogdan8633 <106478911+Bogdan8633@users.noreply.github.com>
2023-02-15 08:53:01 +0000

    Deploying to gh-pages from @ Bogdan8633/goit-react-hw-05-movies@4bfcc6d6b1458b2eefe4423fabd2ab02e0676a71 🚀

---
+++ static/js/29.2f9b8536.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[29],{29:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var a="notFoundPage_message__sFPaN",r=n(184),u=function(){return(0,r.jsx)("h2",{className:a,children:"Not found"})}}}]);
+//# sourceMappingURL=29.2f9b8536.chunk.js.map
